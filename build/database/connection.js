24a290d14a935f0b220579e7e6527befe3ceef0d
Joao <joao@MacBook-Pro-de-Joao-Pedro.local>
2020-12-07 19:48:07 -0300

    added Profile for heroku

--- build/database/connection.js
+++ build/database/connection.js
@@ -12,15 +12,15 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 require('dotenv/config');
 
-if (process.env.NODE_ENV == 'production') {
-  console.log('Productio');
-  _mongoose2.default.connect('mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + '/' + process.env.DB_NAME + '?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-} else {
-  console.log('Dev');
-  _mongoose2.default.connect('mongodb://localhost:27017/ecommerce', {
-    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true
-  });
-}
+// mongosse.connect(
+//   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+//   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+// );
+
+
+_mongoose2.default.connect('mongodb+srv://admin:Simple258079@simplestore.efyb5.mongodb.net/ecommerce?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true
+});
 
 _mongoose2.default.Promise = global.Promise;
 
